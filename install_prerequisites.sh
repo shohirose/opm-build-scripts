475ff6d4cc8ee8c16f7599f4fed4f387a78b5906
Sho Hirose <sho.hirose@gmail.com>
2022-05-05 16:54:36 +0900

    Add ninja-build and libparmetis-dev

--- install_prerequisites.sh
+++ install_prerequisites.sh
@@ -2,12 +2,26 @@
 #
 # Install prerequisites for OPM modules
 
-sudo apt install -y build-essential gfortran pkg-config cmake
+# Packages necessary for building
+sudo apt install -y build-essential gfortran pkg-config cmake ninja-build
+
+# Packages necessary for documentation
 sudo apt install -y doxygen ghostscript texlive-latex-recommended gnuplot
+
+# Packages necessary for version control
 sudo apt install -y git-core
-sudo apt install -y mpi-default-dev libptscotch-dev
+
+# MPI
+sudo apt install -y mpi-default-dev libptscotch-dev libparmetis-dev
+
+# Prerequisites
 sudo apt install -y libblas-dev libboost-all-dev libsuitesparse-dev libtrilinos-zoltan-dev
+
+# Dune modules
 sudo apt install -y libdune-common-dev libdune-geometry-dev libdune-istl-dev libdune-grid-dev
+
+# Optional packages
 sudo apt install -y libcjson-dev cppcheck clang-tools
-# opm-models
+
+# For opm-models
 sudo apt install -y libgmp-dev valgrind libtbb-dev
